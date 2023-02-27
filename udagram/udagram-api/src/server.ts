1cf897bcece12fc244e50066d2f2ebb8ac0e1a14
AhmedKamal29 <ahmed.kamal29@outlook.com>
2023-02-27 18:51:19 +0200

    fixed back and frontend

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -25,7 +25,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = 8080;
+  const port = 3000;
 
   app.use(bodyParser.json());
 
@@ -49,7 +49,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
-    res.send("/api/v0/");
+    res.send("backend server is running");
   });
 
   // Start the Server
