9c130f0ffe7c2a2b2e9ee6b4338b01b0cbd8cbb0
AhmedKamal29 <ahmed.kamal29@outlook.com>
2023-03-02 11:46:51 +0200

    changes in frontend envionment files

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "udagram-api-dev.eba-mzmsqvd6.us-east-1.elasticbeanstalk.com",
+  appName: "udagram-api",
+  apiHost: "http://udagram-api-dev.eba-mzmsqvd6.us-east-1.elasticbeanstalk.com/",
 };
 
 /*
